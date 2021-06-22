da2543c3efec72aff9c8cf7944056c920db0b61c
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-06-22 18:59:42 +0530

    size of datatyes

--- datatypes.cpp
+++ datatypes.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 int main()
 {
-    cout << sizeof(int) << "bytes" endl;
+    int a;
+    int b;
+    float c;
+    double d;
+    char e;
+
+    cout << "Size of a: " << sizeof(a) << endl;
+    cout << "Size of b: " << sizeof(b) << endl;
+    cout << "Size of c: " << sizeof(c) << endl;
+    cout << "Size of d: " << sizeof(d) << endl;
+    cout << "Size of e: " << sizeof(e) << endl;
 }
