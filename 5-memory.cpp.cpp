2c5192d44fbc2573e4db51fdd017d4b338093e64
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-06-23 13:10:26 +0530

    memory

---
+++ 5-memory.cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char alphabet = 'A';
+	cout << "Alphabet" << alphabet << endl;
+	
+	int num_value_of_alphabet = 'A';
+	cout << "Num Value of " << alphabet << ": " << num_value_of_alphabet << endl;
+	
+	// Max and Min Values of Datatyes
+	cout << "Max Value of int: " << INT_MAX << endl;
+	cout << "Min Value of int: " << INT_MIN << endl;
+	cout << "Max value of char: " << CHAR_MAX << endl;
+	cout << "Min value of char: " << CHAR_MIN << endl;
+}
